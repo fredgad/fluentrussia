ce770f51143a0626079defb4762410b19044aa91
fredgad <31152572+fredgad@users.noreply.github.com>
2019-12-22 23:35:01 +0300

    slider commit

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,34 @@
 "use strict";
 
 $(function () {
-  window.addEventListener('scroll', function () {
-    if ($(window).width() < 1024) {
-      var scrolled = window.pageYOffset || document.scrollTop;
-      console.log('asdsad');
+  var educVertical = $('#educational_services'),
+      educGorisontal = $('#education_hor_scroll'),
+      go = 0,
+      educChilds = educGorisontal.children(),
+      educChildsWidth = 30;
 
-      if ($('#numbers').offsetTop()) {
-        console.log($('#numbers').offsetTop());
+  for (var x = 0; x < educChilds.length; x++) {
+    var w = $(educChilds[x]).width() + parseInt($(educChilds[x]).css('margin-right'));
+    educChildsWidth += w;
+  }
+
+  var pointWidth = $(window).width() / ((educChildsWidth + 50) / 100);
+  $('#educ-point').width(pointWidth + '%');
+  educGorisontal.width(educChildsWidth + 50 + 'px');
+  educGorisontal.css('min-width', educChildsWidth + 50 + 'px');
+  educVertical.height(educChildsWidth - 200 + 'px');
+  educVertical.css('min-height', educChildsWidth - 200 + 'px');
+  $(window).on('scroll', function () {
+    if ($(window).width() > 1024) {
+      var scrolled = window.pageYOffset || document.scrollTop,
+          elTop = $('#educational_services').offset().top,
+          partnersTop = $('#partners').offset().top;
+      go = go >= 0 ? scrolled - elTop : 0;
+
+      if (scrolled > elTop && go < $('#education_hor_scroll').width() - $(window).width() + 300) {
+        $('#education_hor_scroll').css('transform', "translate(-".concat(go, "px)"));
+        var goLine = go / ((educGorisontal.width() + pointWidth + 400) / $(window).width());
+        $('#educ-point').css('transform', "translate(".concat(goLine, "px)"));
       }
     }
   });
--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,42 @@
 
-$(function () {
-    
-window.addEventListener('scroll', ()=> {
-    if($(window).width() < 1024) {
-        let scrolled = window.pageYOffset || document.scrollTop;
-        console.log('asdsad')
-        if($('#numbers').offsetTop()) {
-            console.log($('#numbers').offsetTop())
+$(function () { 
+    let educVertical = $('#educational_services'),
+        educGorisontal = $('#education_hor_scroll'),
+        go = 0,
+        educChilds = educGorisontal.children(),
+        educChildsWidth = 30;
+
+    for(let x = 0; x < educChilds.length; x++) {
+        let w = $(educChilds[x]).width() + parseInt($(educChilds[x]).css('margin-right'));
+        educChildsWidth += w;
+    }
+
+    let pointWidth = $(window).width() / ((educChildsWidth+50)/100)
+
+    $('#educ-point').width(pointWidth + '%')
+    educGorisontal.width(educChildsWidth+50 + 'px')
+    educGorisontal.css('min-width', educChildsWidth+50 + 'px')
+    educVertical.height(educChildsWidth-200 + 'px')
+    educVertical.css('min-height', educChildsWidth-200 + 'px')
+
+$(window).on('scroll', ()=> {
+    if($(window).width() > 1024) {
+        let scrolled = window.pageYOffset || document.scrollTop,
+            elTop = $('#educational_services').offset().top,
+            partnersTop = $('#partners').offset().top;
+
+        go =  go >= 0 ? scrolled - elTop : 0;
+
+
+        if(scrolled > elTop && 
+          go < $('#education_hor_scroll').width() - $(window).width() + 300) {
+            $('#education_hor_scroll').css(
+                'transform', `translate(-${go}px)`
+            )
+            let goLine = go / ((educGorisontal.width() + pointWidth + 400) / $(window).width());   
+            $('#educ-point').css(
+                'transform', `translate(${goLine}px)`
+            )
         }
    } 
 });
